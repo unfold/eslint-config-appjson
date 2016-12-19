fb57e09b2d826aac21d46b411be8853b124c997b
Einar Löve <post@einarlove.com>
2016-12-19 14:36:09 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import requireEnvDefined from './rules/require-process-env-defined'
+
+export default {
+  rules: {
+    'require-process-env-defined': requireEnvDefined,
+  },
+}
---
+++ src/rules/require-process-env-defined.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-console */
+
+import fs from 'fs'
+import findUp from 'find-up'
+
+const getAppsjson = context => {
+  const path = findUp.sync('app.json', { cwd: context })
+
+  try {
+    return JSON.parse(fs.readFileSync(path, 'utf8'))
+  } catch (error) {
+    console.log(`Could not parse app.json at ${path}.`)
+    return null
+  }
+}
+
+export default context => {
+  const whitelist = context.options[0] || []
+  const appjson = getAppsjson(context.getFilename())
+
+  if (!appjson || !appjson.env) {
+    return false
+  }
+
+  return {
+    MemberExpression(node) {
+      const objectName = node.object.name
+      const propertyName = node.property.name
+      const isProcessEnv = !node.computed && objectName === 'process' && propertyName === 'env'
+
+      if (isProcessEnv) {
+        const keyNode = node.parent.property
+        const key = keyNode.name || keyNode.value
+        const nodeBefore = context.getTokenBefore(keyNode)
+
+        /* Ignores process.env[key] but applies to process.env['value'] */
+        if (keyNode.type === 'Identifier' && nodeBefore.value !== '.') {
+          return
+        }
+
+        if (!appjson.env[key] && whitelist.indexOf(key) === -1) {
+          context.report(keyNode, `${key} is not declared in your app.json file`)
+        }
+      }
+    },
+  }
+}
